ccd63888dde0fe183f036f20ff5d230c89c3cbad
viswendra <viswendra.95@gmail.com>
2023-07-19 19:26:13 +0530

    added comment [skip ci]

--- src/components/MainContent.test.jsx
+++ src/components/MainContent.test.jsx
@@ -11,6 +11,8 @@ describe('MainContent', () => {
     expect(screen.getByRole('button')).toBeInTheDocument();
   });
 
+  #Adding some sample comment
+
   it('should show the help area after clicking the button', async () => {
     render(<MainContent />);
 
